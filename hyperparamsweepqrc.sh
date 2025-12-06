41afb66618bca875f9781ea0df8779ef434b9001
Punreach Rany <punreach@login3.int.fir.alliancecan.ca>
2025-12-06 15:18:26 -0800

    [output]: qrc first sweep completed - Second sweep is on the way

--- hyperparamsweepqrc.sh
+++ hyperparamsweepqrc.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --job-name=qrc_hyperparam_adam
 #SBATCH --gres=gpu:nvidia_h100_80gb_hbm3_1g.10gb:1
-#SBATCH --time=12:00:00
+#SBATCH --time=05:00:00
 #SBATCH --cpus-per-task=12
 #SBATCH --mem=32G
 #SBATCH --output=output_qrc_change.log
@@ -15,6 +15,6 @@ module load cuda/12.6
 
 source cc/bin/activate
 echo "starting python"
-python CC_Sweep/sweep.py --agent qrc --seeds 9999 --jobs 12 --output 'data/qrc_changed_sweep_results'
+python CC_Sweep/sweep.py --agent qrc --seeds 9999 --jobs 12 --output 'data/qrc_sweep_results'
 
 echo "finished"
